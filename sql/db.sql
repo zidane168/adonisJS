b229efe316a6a4c61815faa4dcfa72b2bf2e7d25
zidane <huuvi168@gmail.com>
2022-08-28 15:46:22 +0700

    add db

--- sql/db.sql
+++ sql/db.sql
@@ -1,10 +1,40 @@
- 
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 28, 2022 at 10:45 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `adonisjs`
+--
+CREATE DATABASE IF NOT EXISTS `adonisjs` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `adonisjs`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adonis_schema`
+--
+
 DROP TABLE IF EXISTS `adonis_schema`;
 CREATE TABLE IF NOT EXISTS `adonis_schema` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `batch` int(11) NOT NULL,
-  `migration_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `migration_time` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
@@ -13,8 +43,8 @@ CREATE TABLE IF NOT EXISTS `adonis_schema` (
 --
 
 INSERT INTO `adonis_schema` (`id`, `name`, `batch`, `migration_time`) VALUES
-(1, 'database/migrations/1656054685502_users', 1, '2022-06-24 07:47:24'),
-(2, 'database/migrations/1656056680827_pets', 1, '2022-06-24 07:47:24');
+(1, 'database/migrations/1656054685502_users', 1, '2022-06-24 00:47:24'),
+(2, 'database/migrations/1656056680827_pets', 1, '2022-06-24 00:47:24');
 
 -- --------------------------------------------------------
 
@@ -33,6 +63,25 @@ CREATE TABLE IF NOT EXISTS `adonis_schema_versions` (
 
 INSERT INTO `adonis_schema_versions` (`version`) VALUES
 (2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `followings`
+--
+
+DROP TABLE IF EXISTS `followings`;
+CREATE TABLE IF NOT EXISTS `followings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `following_id` int(11) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT 1,
+  `created` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -60,13 +109,22 @@ DROP TABLE IF EXISTS `posts`;
 CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  `image` varchar(1024) DEFAULT NULL,
+  `caption` text DEFAULT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT 1,
   `created` datetime DEFAULT NULL,
   `created_by` int(11) DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
   `updated_by` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `image`, `caption`, `enabled`, `created`, `created_by`, `updated`, `updated_by`) VALUES
+(1, 1, 'posts/1661676244687.jpg', 'Ngoc TRinhb', 1, '2022-08-28 15:44:04', NULL, '2022-08-28 15:44:04', NULL);
 
 -- --------------------------------------------------------
 
@@ -78,6 +136,8 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT 1,
+  `bio` text DEFAULT NULL,
   `avatar` varchar(1024) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(180) NOT NULL,
@@ -93,11 +153,11 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `avatar`, `email`, `password`, `remember_me_token`, `created`, `updated`) VALUES
-(1, 'vilh', 'images/1660645290958.gif', '0', '$argon2id$v=19$t=3,m=4096,p=1$D/deO7zIXC1LU1R0mih3Fg$PRK33Pmtrum0s2n77AaSsAVc68DTXcXg9lBB89NZGz0', NULL, '2022-07-29 11:01:19', '2022-08-16 17:21:30'),
-(3, 'zidane', NULL, 'zidane@gmail.com', '$argon2id$v=19$t=3,m=4096,p=1$cu9QDg0lz9r/tbB11jAdDg$jggcp6qi5Bm4jQ8G44+hQH6lPMD33ISwmExptHMteqE', NULL, '2022-08-16 14:25:57', '2022-08-16 14:25:57');
+INSERT INTO `users` (`id`, `username`, `enabled`, `bio`, `avatar`, `email`, `password`, `remember_me_token`, `created`, `updated`) VALUES
+(1, 'vilh', 1, NULL, 'images/1660645290958.gif', '0', '$argon2id$v=19$t=3,m=4096,p=1$D/deO7zIXC1LU1R0mih3Fg$PRK33Pmtrum0s2n77AaSsAVc68DTXcXg9lBB89NZGz0', '0t7bhhj0jh7ULn1UWE9d', '2022-07-29 11:01:19', '2022-08-28 15:41:20'),
+(3, 'zidane', 1, NULL, NULL, 'zidane@gmail.com', '$argon2id$v=19$t=3,m=4096,p=1$cu9QDg0lz9r/tbB11jAdDg$jggcp6qi5Bm4jQ8G44+hQH6lPMD33ISwmExptHMteqE', NULL, '2022-08-16 14:25:57', '2022-08-16 14:25:57');
+COMMIT;
 
-
-ALTER TABLE `users` ADD `bio` TEXT NULL AFTER `username`;
-ALTER TABLE `posts` ADD `image` VARCHAR(1024) NULL AFTER `user_id`, ADD `caption` TEXT NULL AFTER `image`;
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
